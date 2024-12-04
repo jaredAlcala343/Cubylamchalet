cf85be1363f07bf2ac53b8aad11f689ffbce5e45
jaredAlcala343 <jalcalaing@gmail.com>
2024-12-04 17:08:00 -0600

    000002
    commit correctivo y se agrega el about us

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./About.css";
 
-function About() {
+const About = () => {
+    const [selectedInfo, setSelectedInfo] = useState("Quienes Somos");
+    const [fade, setFade] = useState(false);
+
+    const handleButtonClick = (info) => {
+        setFade(true);
+        setTimeout(() => {
+            setSelectedInfo(info);
+            setFade(false);
+        }, 300);
+    };
+
     return (
         <div className="container">
             <div className="row">
-                <div className="col">
-                    <h1>About</h1>
+                <ul className="button-list">
+                    <li><button onClick={() => handleButtonClick("Quienes Somos")}>Quienes Somos</button></li>
+                    <li><button onClick={() => handleButtonClick("Mision")}>Mision</button></li>
+                    <li><button onClick={() => handleButtonClick("Vision")}>Vision</button></li>
+                    <li><button onClick={() => handleButtonClick("Historia")}>Historia</button></li>
+                </ul>
+            </div>
+            <div className={`info-container ${fade ? 'fade' : ''}`}>
+                <div className="info">
+                    {selectedInfo === "Quienes Somos" && (
+                        <div>
+                            <h2>Quienes Somos</h2>
+                            <p>Información sobre quienes somos.</p>
+                        </div>
+                    )}
+                    {selectedInfo === "Mision" && (
+                        <div>
+                            <h2>Mision</h2>
+                            <p>Nuestra misión es...</p>
+                        </div>
+                    )}
+                    {selectedInfo === "Vision" && (
+                        <div>
+                            <h2>Vision</h2>
+                            <p>Nuestra visión es...</p>
+                        </div>
+                    )}
+                    {selectedInfo === "Historia" && (
+                        <div>
+                            <h2>Historia</h2>
+                            <p>Nuestra historia comenzó...</p>
+                        </div>
+                    )}
                 </div>
-            </div>            
-        </div>        
+            </div>
+        </div>
     );
-}
+};
 
 export default About;
